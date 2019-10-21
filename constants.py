a620bcb384735cfac14ecf08bd28219d05c0ecbf
Ubuntu <ubuntu@ip-172-31-24-146.ec2.internal>
2019-10-21 05:08:21 +0000

    send file to git hub

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Mariam Hemdan
+# Created on: October 2019
+# This constants file for Space Alien game
+
+# CircuitPython scern size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCEEN_GRID_X = 10
+SCEEN_GRID_Y = 8
+SPRITE_SIZE = 16
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
